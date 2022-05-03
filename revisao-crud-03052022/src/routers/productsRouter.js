8d056e5dab93093d7af48fb1197a6be6409c6b47
Cassio Oliveira Silva <cos@vortx.com.br>
2022-05-03 15:39:11 -0300

    Adicionado o projeto de revisão de CRUD

---
+++ revisao-crud-03052022/src/routers/productsRouter.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const ProductsController = require('../controllers/ProductsController')
+
+const router = express.Router()
+
+router.get('/', ProductsController.index)
+router.get('/detail/:id', ProductsController.details)
+
+// TODO
+// router.get('/edit/:id', ProductController.edit)
+// router.patch('/edit/:id', ProductController.update)
+
+module.exports = router
